949fe6e120bf1113d997faeeb4cecac9f20cc309
eloBernard <160846426+eloBernard@users.noreply.github.com>
2024-04-29 15:22:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import os
-print("HAS create and test python image using docker"")
+print("HAS create and test python image using docker and jenkins build"")
 print("HAS/SSI python app test current Directory : ",os.getcwd())
